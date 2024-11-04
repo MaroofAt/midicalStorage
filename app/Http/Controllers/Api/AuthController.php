91d14da805887b2bddf29c202a5149bade7cfbb7
MaroofAt <maroof95at@gmail.com>
2024-11-04 15:51:40 +0300

    Auth Controller: (login , logout , refresh) | Changed Middleware name from JWTchangelang to LangMiddleware | ResponseTrait Updated | api.php Updated

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,20 +3,53 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Tools\ResponseTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    use ResponseTrait;
+
     public function register(){
-        // TODO..
+        // TODO.. (Ali)
     }
-    public function login(){
-        // TODO..
+
+    public function login(Request $request){
+        // validation
+        $validator = Validator::make($request->all(),[
+            'email' => ['required' , 'email' , 'unique:users' , 'exists:users,email'],
+            'password' => ['required' , 'min:8'],
+        ]);
+        if($validator->fails()){
+            return $this->response($validator->errors() , 400 , 'Validation Error : Some Information is wrong');
+        }
+
+        // attempt
+        $token = auth('api')->attempt($validator->validated());
+        if(!$token){
+            return $this->response(null , 500 , 'JWT error: can\'t generate token');
+        }
+
+        // response
+        return $this->response([
+            'access_token' => $token,
+            'expire_date' => auth('api')->factory()->getTTl(),
+        ] , 200 , 'Logined Successfully');
     }
+
     public function logout(){
-        // TODO..
+        auth('api')->logout();
+        return $this->response(null , 200 , 'Logged out Successfully');
     }
+
     public function me(){
-        // TODO..
+        // TODO.. (Ali)
     }
+
+    public function refresh()
+    {
+        return $this->response(auth()->refresh());
+    }
+
 }
