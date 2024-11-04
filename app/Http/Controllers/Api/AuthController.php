fecf64e6617b824ff55ef2b1f0ab7bcf038942b9
ali almsfi <AliAlmusfi>
2024-11-04 15:51:17 +0300

    AithController modifing the register and me functions | make seeds for all models in DatabaseSeeder

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,21 +2,43 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Tools\ResponseTrait;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function register(){
-        // TODO..
+    use ResponseTrait;
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|email|string|max:100|unique:users',
+            'password' => 'required|password|string|min:8'
+        ]);
+        if ($validator->fails()) {
+            return $this->response("Exception Error", 400, $validator->error());
+        }
+        $user = User::create($validator->validated());
+        if(!$user){
+            return $this->response(null, 500 , "Can't Create User");
+        }
+        return $this->response($user , 200 , "registered Successfully");
+
+
     }
-    public function login(){
-        // TODO..
+    public function login()
+    {
+        // TODO.. maroof
     }
-    public function logout(){
-        // TODO..
+    public function logout()
+    {
+        // TODO.. maroof
     }
-    public function me(){
-        // TODO..
+    public function me()
+    {
+        return $this->response(auth('api')->user());
     }
 }
