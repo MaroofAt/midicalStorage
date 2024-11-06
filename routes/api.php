fe4d2cb5672dbebe4f9bbccd4c6903d926c9a1cf
MaroofAt <maroof95at@gmail.com>
2024-11-06 14:48:50 +0300

    CategoryController CRUD + get_one() method and thier routs

--- routes/api.php
+++ routes/api.php
@@ -6,20 +6,34 @@ use App\Http\Middleware\LangMiddleware;
 use App\Http\Middleware\APIPassMiddleware;
 use App\Http\Middleware\JWTPassMiddleware;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Middleware\TokenMiddleware;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 Route::middleware(APIPassMiddleware::class , LangMiddleware::class)->group(function(){
+
     Route::middleware('api')->prefix('auth')->group(function(){
         Route::post('/register' , [AuthController::class, 'register']);
         Route::post('/login' , [AuthController::class, 'login']);
+
         Route::middleware(TokenMiddleware::class , 'auth:api')->group(function(){
             Route::post('/logout' , [AuthController::class, 'logout']);
             Route::post('/me' , [AuthController::class, 'me']);
             Route::post('/refresh' , [AuthController::class, 'refresh']);
         });
+
     });
+
+
+    Route::prefix('category')->group(function(){
+        Route::post('/show' , [CategoryController::class , 'show']);
+        Route::post('/store' , [CategoryController::class , 'store']);
+        Route::post('/update' , [CategoryController::class , 'update']);
+        Route::post('/destroy' , [CategoryController::class , 'destroy']);
+        Route::post('/get_one' , [CategoryController::class , 'get_one']);
+    });
+
 });
 
