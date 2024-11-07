80d2a9bf26434fa3a3cf9f24b1c8b56ee587dccf
ali almsfi <AliAlmusfi>
2024-11-07 14:33:26 +0300

    Make UserController | Make Api for User and Company

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Middleware\LangMiddleware;
 use App\Http\Middleware\APIPassMiddleware;
 use App\Http\Middleware\JWTPassMiddleware;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\MedicineController;
 
 // Route::get('/user', function (Request $request) {
@@ -26,5 +28,22 @@ Route::middleware(APIPassMiddleware::class, LangMiddleware::class)->group(functi
         Route::post('/destroy', [MedicineController::class, 'destroy']);
         Route::post('/show_one_medicine', [MedicineController::class, 'show_one_medicine']);
         Route::post('/show_by_category', [MedicineController::class, 'show_by_category']);
+    });
+
+
+    Route::prefix('company')->group(function () {
+        Route::post('/show', [CompanyController::class, 'show']);
+        Route::post('/store', [CompanyController::class, 'store']);
+        Route::post('/update', [CompanyController::class, 'update']);
+        Route::post('/destroy', [CompanyController::class, 'destroy']);
+        Route::post('/get_one', [CompanyController::class, 'get_one']);
+    });
+
+
+
+    Route::prefix('user')->group(function () {
+        Route::post('/show', [UserController::class, 'show']);
+        Route::post('/show_one', [UserController::class, 'show_one']);
+        Route::post('/update', [UserController::class, 'update']);
     });
 });
